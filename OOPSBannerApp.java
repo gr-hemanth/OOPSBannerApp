86299fedcc3d3946216ff201d90edb9312df6e99
avanyasahu8-ux <avanyasahu8@gmail.com>
2026-02-17 15:07:23 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,4 @@
-/**
- * OOPSBannerApp
- *
- * UC2: Render OOPS as Banner using Print Statements
- *
- * @author Hemanth
- * @version 2.0
- */
-public class OOPSBannerApp {
+ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
