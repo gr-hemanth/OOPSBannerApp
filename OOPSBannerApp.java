59e6adac11e68f5c9042c15d2b9e070a591de6f6
Hemanth <grhemanth123@gmail.com>
2026-02-27 22:54:17 +0530

    Added UC5: Inline array

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,24 @@
 /**
  * OOPSBannerApp
  *
- * UC4: Render OOPS as Banner using String Array and Loop
+ * UC5: Render OOPS as Banner using Inline Array Initialization
  *
  * @author Hemanth
- * @version 4.0
+ * @version 5.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
-
-        lines[0] = String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** ");
-        lines[1] = String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    ");
-        lines[2] = String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    ");
-        lines[3] = String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   ");
-        lines[4] = String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  ");
-        lines[5] = String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  ");
-        lines[6] = String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** ");
+        String[] lines = {
+            String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** "),
+            String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    "),
+            String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    "),
+            String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   "),
+            String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  "),
+            String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  "),
+            String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** ")
+        };
 
         for (String line : lines) {
             System.out.println(line);
